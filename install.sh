e451ad4b4961c24a5d11482e27305575fbee243c
Steven Normore <snormore@gmail.com>
2014-04-08 19:31:55 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ BIN_DIR=/usr/local/bin
 set -xe
 mkdir -p $SRC_DIR && cd $SRC_DIR
 curl -0 https://raw.githubusercontent.com/snormore/goenv/master/goenv.sh > goenv.sh
-go build -a -o goenv
-cp goenv $BIN_DIR/goenv
+cp goenv.sh $BIN_DIR/goenv
 chmod +x $BIN_DIR/goenv
